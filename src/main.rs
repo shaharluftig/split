73feea0ae240a05946e2ca8fcf2048a47b056955
Luftig <shaharluftig@gmail.com>
2022-09-26 18:47:53 +0300

    Missing types added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 
+use crate::models::args::Args;
+
 mod models;
 mod utils;
 
 fn main() {
-    let args = models::args::Args::parse();
+    let args: Args = models::args::Args::parse();
     utils::splitter::split_file(&args.path, args.lines, args.ignore_empty_lines);
 }
