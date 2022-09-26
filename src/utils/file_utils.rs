48ad6bcdd922460c66da6781fea2376fd835a970
Luftig <shaharluftig@gmail.com>
2022-09-26 16:05:27 +0300

    reorder functions

--- src/utils/file_utils.rs
+++ src/utils/file_utils.rs
@@ -3,20 +3,23 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::PathBuf;
 
-pub fn create_file_buffer(path: &PathBuf) -> BufWriter<File> {
+/// Create a new file and returns a buffer to the file
+pub fn create_write_file_buffer(path: &PathBuf) -> BufWriter<File> {
     let mut file = match File::create(&path) {
         Err(err) => panic!("Couldn't write {file_name}:{reason}",
                            file_name = &path.file_name().and_then(OsStr::to_str).unwrap(), reason = err),
         Ok(file) => file,
     };
+    println!("File:{} written successfully", &path.file_name().and_then(OsStr::to_str).unwrap());
     BufWriter::new(file)
 }
 
-pub fn read_file(path: &PathBuf) -> File {
+/// Reads a file and returns a buffer to the file
+pub fn create_read_file_buffer(path: &PathBuf) -> BufReader<File> {
     let mut file = match File::open(&path) {
         Err(err) => panic!("Couldn't open {file_name}:{reason}",
                            file_name = &path.file_name().and_then(OsStr::to_str).unwrap(), reason = err),
         Ok(file) => file,
     };
-    file
+    BufReader::new(file)
 }
--- src/utils/splitter.rs
+++ src/utils/splitter.rs
@@ -6,23 +6,23 @@ use std::path::PathBuf;
 use crate::utils::file_utils;
 
 pub fn split_file(path: &PathBuf, lines: usize, files: usize, ignore_empty_lines: bool) {
-    let file = file_utils::read_file(&path);
-    let reader: BufReader<File> = BufReader::new(file);
+    let file_read_buffer: BufReader<File> = file_utils::create_read_file_buffer(&path);
     let mut file_index: usize = 1;
-    let mut file_buffer: BufWriter<File> = file_utils::create_file_buffer(&format_path(&path, &file_index));
+    let mut file_writer_buffer: BufWriter<File> = file_utils::create_write_file_buffer(
+        &format_path(&path, &file_index));
 
-    for (line_index, line) in reader.lines().enumerate() {
-        let row_num: usize = &line_index + 1;
+    for (line_index, line) in file_read_buffer.lines().enumerate() {
+        let line_num: usize = &line_index + 1;
         let line_value: String = line.unwrap();
         if ignore_empty_lines && &line_value == "" {
             continue;
         }
-        writeln!(file_buffer, "{}", &line_value).expect(format!("Unable to write line:{}", row_num).as_str());
-        if row_num % &lines == 0 && row_num != 0 {
+        writeln!(file_writer_buffer, "{}", &line_value).expect(format!("Unable to write line:{}", line_num).as_str());
+        if line_num % &lines == 0 && line_num != 0 {
             file_index = &file_index + 1;
             let formatted_path: PathBuf = format_path(&path, &file_index);
-            file_buffer = file_utils::create_file_buffer(&formatted_path);
-            println!("File:{} written successfully", &formatted_path.file_name().and_then(OsStr::to_str).unwrap());
+            file_writer_buffer = file_utils::create_write_file_buffer(&formatted_path);
+
         }
     }
 }
