b0ad7ac3547068c8122c0e5b89746f789b133fb7
Luftig <shaharluftig@gmail.com>
2022-09-26 15:16:22 +0300

    better error msgs

--- src/file_utils/splitter.rs
+++ src/file_utils/splitter.rs
@@ -4,7 +4,7 @@ use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::PathBuf;
 
 pub fn split_file(path: &PathBuf, lines: usize, files: usize, ignore_empty_lines: bool) {
-    let reader = BufReader::new(File::open(path).expect("Cannot open file"));
+    let reader: BufReader<File> = BufReader::new(File::open(path).expect("Cannot open file"));
     let mut file_index: usize = 1;
     let mut file_buffer: BufWriter<File> = create_file_buffer(format_path(&path, file_index));
 
@@ -14,7 +14,7 @@ pub fn split_file(path: &PathBuf, lines: usize, files: usize, ignore_empty_lines
         if ignore_empty_lines && &line_value == "" {
             continue;
         }
-        writeln!(file_buffer, "{}", &line_value).expect("Unable to write line");
+        writeln!(file_buffer, "{}", &line_value).expect(format!("Unable to write line:{}", row_num).to_str());
         if row_num % &lines == 0 && row_num != 0 {
             file_index = &file_index + 1;
             file_buffer = create_file_buffer(format_path(&path, file_index));
@@ -32,6 +32,8 @@ fn format_path(path: &PathBuf, file_number: usize) -> PathBuf {
 }
 
 fn create_file_buffer(path: PathBuf) -> BufWriter<File> {
-    let file = File::create(&path).expect("unable to create file");
+    let file = File::create(&path)
+        .expect(format!("unable to create {}",
+                        &path.file_name().and_then(OsStr::to_str).unwrap()).as_str());
     return BufWriter::new(file);
 }
