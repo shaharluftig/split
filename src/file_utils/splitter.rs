c99b689d969f43e7eb4d287e37fdcdfc156cfd68
Luftig <shaharluftig@gmail.com>
2022-09-26 15:04:48 +0300

    flag added

--- src/file_utils/splitter.rs
+++ src/file_utils/splitter.rs
@@ -3,14 +3,19 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::PathBuf;
 
-pub fn split_file(path: &PathBuf, lines: usize, files: usize) {
+pub fn split_file(path: &PathBuf, lines: usize, files: usize, ignore_empty_lines: bool) {
     let reader = BufReader::new(File::open(path).expect("Cannot open file"));
     let mut file_index: usize = 1;
-    let mut file_buffer = create_file_buffer(format_path(&path, file_index));
+    let mut file_buffer: BufWriter<File> = create_file_buffer(format_path(&path, file_index));
 
     for (line_index, line) in reader.lines().enumerate() {
-        writeln!(file_buffer, "{}", line.unwrap()).expect("Unable to write line");
-        if line_index % &lines == 0 && line_index != 0 {
+        let row_num: usize = &line_index + 1;
+        let line_value: String = line.unwrap();
+        if ignore_empty_lines && &line_value == "" {
+            continue;
+        }
+        writeln!(file_buffer, "{}", &line_value).expect("Unable to write line");
+        if row_num % &lines == 0 && row_num != 0 {
             file_index = &file_index + 1;
             file_buffer = create_file_buffer(format_path(&path, file_index));
         }
@@ -22,7 +27,7 @@ fn format_path(path: &PathBuf, file_number: usize) -> PathBuf {
                        file_name = &path.file_stem().and_then(OsStr::to_str).unwrap(),
                        index = file_number.to_string(),
                        file_stem = &path.extension().and_then(OsStr::to_str).unwrap());
-    println!("file:{} written successfully", path);
+    println!("File:{} written successfully", path);
     PathBuf::from(path)
 }
 
--- src/models/args.rs
+++ src/models/args.rs
@@ -1,16 +1,19 @@
 use clap::Parser;
 
-/// Split a file to multiply files
+/// Split any file to multiply files
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// lines per output file
+    /// Number of lines per output file
     #[clap(short, long, value_parser, default_value_t = 1000)]
     pub lines: usize,
-    /// number of output files
+    /// Number of output files
     #[clap(short, long, value_parser, default_value_t = 1)]
     pub files: usize,
     /// The path to the file to read
     #[clap(parse(from_os_str))]
     pub path: std::path::PathBuf,
+    /// Ignore empty lines,
+    #[clap(long, value_parser, default_value_t = false)]
+    pub ignore_empty_lines: bool,
 }
