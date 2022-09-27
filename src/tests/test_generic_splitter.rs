1e22a9fa8fddaaefcf72ed5040f2a3af60dcf38e
Luftig <shaharluftig@gmail.com>
2022-09-27 19:57:12 +0300

    test boilerplate added

---
+++ src/tests/test_generic_splitter.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_split_to_lines() {
+        assert_eq!(1 + 2, 3);
+    }
+}
