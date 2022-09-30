edf467c2cc9cf062ae0815bc7accd38768acc4df
Luftig <shaharluftig@gmail.com>
2022-09-30 17:26:13 +0300

    change bin (#17)

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod utils;
 mod tests;
 
 fn main() {
-    let now = Instant::now();
+    let now: Instant = Instant::now();
     let args: Args = models::args::Args::parse();
     splitter::generic_splitter::split_file(&args.path, args.lines, args.ignore_empty_lines);
     println!("Finished, time took: {} milliseconds", now.elapsed().as_millis());
